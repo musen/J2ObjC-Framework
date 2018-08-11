465fb02e0ba36779516eccd6a4656bf859825731
musen <musen@users.noreply.github.com>
2018-08-11 15:31:30 +0300

    Update download.sh

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -6,7 +6,7 @@ sha1_checksum=6f2f45b323ef92cc8d1b13dd789aebd1ab87562f
 
 echo "fetching j2objc dist"
 curl -OL http://localhost/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
-//echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
+echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} Distributive
 rm j2objc-${j2objc_version}.zip
